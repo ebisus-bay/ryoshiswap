5d70a7736ab79bf103f306a0ce284664cf67332c
Nathan Schwermann <schwiz@gmail.com>
2024-03-25 22:14:44 -0500

    deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,18 +18,17 @@ const config: HardhatUserConfig = {
       chainId: 25,
       accounts: process.env.SIGNER !== undefined ? [process.env.SIGNER] : [],
       // useLedger: true,
-      // factory : '',
-      // wcro : '0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23',
+      factory : '',
+      wcro : '0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23',
     },
     testnet_cronos : {
       url : "https://rpc.ebisusbay.biz/",
       chainId : 338,
       accounts:  process.env.SIGNER !== undefined ? [process.env.SIGNER] : [],
-      // factory : '0x96E508A6d754F2C308E362D9C451B23e21B14907',
-      // wcro: '0x467604E174c87042fcc4412c5BC70AaBc8733016',
-      // router : '0x94352b0eA8C35B52A42c6422fA81F5A78b71693A',
-      // usdc : '0x1e1d0765439d0d53ee40cc4fb454c2343c84342b',
-      // multicall : '0x78eb7e96abe54802b4eabcad57b90fe3bc2b95b7'
+      factory : '0x48fcc39b4249fdd6e1829d54fc30fa809ebb473f',
+      wcro: '0x467604E174c87042fcc4412c5BC70AaBc8733016',
+      router : '0x29c852d866957c029B616c3013D3D015df32A6C7'
+
       // useLedger: false,
     }
   },
@@ -56,29 +55,31 @@ const config: HardhatUserConfig = {
   paths: {
     tests: "./src/test",
   },
-  // etherscan: {
-  //   apiKey: {
-  //     cronos: process.env.CRONOS_API_KEY,
-  //     testnet_cronos: process.env.CRONOS_API_KEY,
-  //   },
-  //   customChains: [
-  //     {
-  //       network: "cronos",
-  //       chainId: 25,
-  //       urls: {
-  //         apiURL: "https://api.cronoscan.com/api",
-  //         browserURL: "https://cronoscan.com/",
-  //       },
-  //     },{
-  //       network: "testnet_cronos",
-  //       chainId : 338,
-  //       urls:{
-  //         apiURL: "https://explorer-api.cronos.org/testnet/api/v1/hardhat/contract?apikey=" + process.env.CRONOS_API_KEY,
-  //         browserURL : "http://explorer.cronos.org/testnet"
-  //       }
-  //     }
-  //   ]
-  //  }
+
+  etherscan: {
+    apiKey: {
+      cronos: process.env.CRONOS_API_KEY,
+      testnet_cronos: process.env.CRONOS_TEST_API_KEY,
+    },
+    customChains: [
+      {
+        network: "cronos",
+        chainId: 25,
+        urls: {
+          apiURL: "https://api.cronoscan.com/api",
+          browserURL: "https://cronoscan.com/",
+        },
+      },{
+        network: "testnet_cronos",
+        chainId : 338,
+        urls:{
+          apiURL: "https://explorer-api.cronos.org/testnet/api/v1/hardhat/contract?apikey=" + process.env.CRONOS_TEST_API_KEY,
+          browserURL : "http://explorer.cronos.org/testnet"
+        }
+      }
+    ]
+   }
+
 };
 
 export default config;
---
+++ src/tasks/deployRouter.js
@@ -0,0 +1,33 @@
+
+// This is a script to deploy the Router contract to the network.
+const hre = require("hardhat");
+const {delay} = require("./utils");
+
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+    const wcroAddress = hre.config.networks[hre.network.name].wcro;
+    const factoryAddress = hre.config.networks[hre.network.name].factory;
+    const signerAddress = deployer.address;
+    console.log("Deploying contracts with the account:", signerAddress);
+    const Router = await hre.ethers.getContractFactory("RyoshiRouter");
+    const router = await Router.deploy(factoryAddress, wcroAddress);
+    
+    await router.waitForDeployment();
+    const routerAddress = await router.getAddress();
+    console.log("Router deployed to:", routerAddress);
+
+    console.log("Verifying Router contract");
+    await delay(7000);
+    await hre.run("verify:verify", {
+        address: routerAddress,
+        constructorArguments: [factoryAddress, wcroAddress],
+    });
+}
+
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
